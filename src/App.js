f075c7f36ecd60b27db8529ce5dc209583c63b09
anoushka <prasadanoushka03@gmail.com>
2023-10-14 11:19:37 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import Profile from './name';  
+
+
+function App() {
+  const [showImages, setShowImages] = useState(false);
+
+  const handleShowImages = () => {
+    setShowImages(true);
+  };
+
+  return (
+    <div className="App">
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Profile</a>
+          </li>
+          <li>
+            <a href="#" onClick={handleShowImages}>
+              Works
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <Profile />
+     
+    </div>
+  );
+}
+
+export default App;
